147f175f8010471f84c2812c9eee3e33f592b3ea
centron2 <duiliomanuelibanez@gmail.com>
2024-11-20 21:55:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-    // Incluir la cabecera y el encabezado si lo deseas modular
-    include 'layaout/head.php';
-    include 'layaout/header.php';
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -13,42 +7,132 @@
     <title>Bienvenidos a nuestra escuela</title>
     <link rel="stylesheet" href="contacto2.css">
 
+
+     <style>
+       
+       
+.carousel {
+    overflow: hidden;
+    width: 100%; /* El contenedor debe ocupar el 100% del ancho de la pantalla */
+}
+
+.marquee-container {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start; /* Asegura que las imágenes estén alineadas a la izquierda */
+    width: 100%; /* Asegura que el contenedor ocupe el 100% de la pantalla */
+}
+
+.marquee-content {
+    display: flex;
+    flex-wrap: nowrap; /* Esto asegura que las imágenes se alineen horizontalmente sin que se apilen */
+    animation: scroll 20s linear infinite;
+}
+
+.marquee img {
+    width: 250px;
+    height: 250px;
+    margin-right: 10px; /* Espacio entre las imágenes */
+}
+
+@keyframes scroll {
+    from {
+        transform: translateX(0);
+    }
+    to {
+        transform: translateX(-100%);
+    }
+}
+
+
+    </style>
+</head>
+
+<body>
+
+    <?php include 'layaout/head.php';?>
+    <?php include 'layaout/header.php';?>
+
+    <div class="hero">
+        <div class="container text-center">
+            <h1>Bienvenidos a nuestra escuela</h1>
+        </div>
+    </div>
+
+ <section class="carousel">
+    <div class="marquee-container">
+        <div class="marquee-content">
+            <div class="marquee">
+                <img src="IMG/bienvenida/21septiembre.jpg" alt="21 de Septiembre">
+                <img src="IMG/bienvenida/albañileria.jpg" alt="Albañilería">
+                <img src="IMG/bienvenida/carpinteria.png" alt="Carpintería">
+                <img src="IMG/bienvenida/informatica.jpg" alt="Informática">
+                <img src="IMG/bienvenida/peluquieria.jpg" alt="Peluquería">
+
+                
+                <!--<img src="IMG/bienvenida/soldadura.jpg" alt="Soldadura">
+                <img src="IMG/bienvenida/electricidad.jpg" alt="Electricidad">
+                <img src="IMG/bienvenida/gastronomia.jpg" alt="Gastronomía">
+                <img src="IMG/bienvenida/horticola.jpg" alt="Hortícola">-->
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+    <br><br>
+    <h2 class="diario-title">Noticias de la Semana</h2>
+    <br>
+
+    <section class="noticias-containersecundario">
+        <article>
+            <img src="/img2.0/construccion.jpg" alt="Proceso de construccion">
+            <h3>Proceso de Construccion<em></em></h3>
+            <p>Avances en la construcción nueva aula CFPN2, en articulación con la Municipalidad de Santa María, continuamos hacia delante para concretar la meta de tener un nuevo espacio para la enseñanza. #fpcatamarca #JuntosPodemos</p>
+            <a href="https://www.facebook.com/share/p/1M8sXMLUnH/" target="_blank">Leer más</a>
+        </article>
+
+        <article>
+            <img src="/img2.0/halloween.jpg" alt="Halloween">
+            <h3>Halloween<em></em></h3>
+            <p>Propuesta para niños Taller de Halloween, (hasta 10 años), aprenderán hacer (cupcakes y galletas), días Jueves primer grupo y viernes segundo grupo, desde las 15:00:h. Valor $2.500 incluye insumos. te esperamos Vení disfrazado.!! p/d inscripciones a los números que figuran el el flyer.</p>
+            <a href="https://www.facebook.com/share/p/15MnXWt92y/" target="_blank">Leer más</a>
+        </article>
+
+        <article>
+            <img src="/img2.0/soldadura.jpg" alt="Soldadura">
+            <h3>Soldadura Industrial</h3>
+            <p>Alumnos de Soldadura (perfil industrial), Prof. Ariel Gerban, preparándose para la calificación de Soldadores, acuerdo entre Minera MARA - INTI - CFPN2. #juntospodemos #fpcatamarca #inet #CFPN2 #inti</p>
+            <a href="https://www.facebook.com/share/p/12Akutpt9Ha/">Leer más</a>
+        </article>
+    </section>
+
+    <section class="noticias-containersecundario">
+        <article>
+            <img src="/img2.0/silicon.jpg" alt="Silicon Valley">
+            <h3>Experencia Silicon Valley<em></em></h3>
+            <p>Participantes de la Experiencia Silicon Valley 2024 propuestos por la Fundación del Tucumán, con integrantes de Paraguay, Bolivia  y Argentina ( Salta Jujuy, Tucuman y Catamrca), Visitas a Google, HP, Plug and Play, Tesla, Stanford University, Amazon,Draper University, Linckein, SAP, Autodesk, Indie Bio, mas otras aceleradores de Startaups, tecnológicas y de biotecnologicas. Nodo Tecnológico- Catamarca Capital Municipalidad de Santa María #FPCatamarca #CFP N2</p>
+            <a href="https://www.facebook.com/share/p/1EiPfLNCHs/" target="_blank" >Leer más</a>
+        </article>
+
+        <article>
+            <img src="/img2.0/diaEstu2.jpg" alt="Jornada dia del estudiante">
+            <h3>Jornada dia del Estudiante<em></em></h3>
+            <p>Jornada por el Día del estudiante y elección del la Reina y princesas. Agradecemos al complejo  policial por brindarnos sus instalaciones </p>
+            <a href="https://www.facebook.com/share/p/19ChxLjDsX/" target="_blank" >Leer más</a>
+        </article>
+
+        <article>
+            <img src="/img2.0/energiaReno.jpg" alt="Energia Renovable">
+            <h3>Energia Renovable</h3>
+            <p>Jornada de capacitación para alumnos IES de la Tecnicatura Superior en Gestión de Energías Renovables, dictado por ell Prof. Fernando Paz y alumnos de Electricidad Domiciliaria. Articulación IES/CFPN2.</p>
+            <a href="https://www.facebook.com/share/p/14k2aGB6bG/"  target="_blank" >Leer más</a>
+        </article>
+    </section>
+
     <style>
-        /* Estilos originales del código */
-
-        .carousel {
-            overflow: hidden;
-            width: 100%; /* El contenedor debe ocupar el 100% del ancho de la pantalla */
-        }
-
-        .marquee-container {
-            display: flex;
-            align-items: center;
-            justify-content: flex-start; /* Asegura que las imágenes estén alineadas a la izquierda */
-            width: 100%; /* Asegura que el contenedor ocupe el 100% de la pantalla */
-        }
-
-        .marquee-content {
-            display: flex;
-            flex-wrap: nowrap; /* Esto asegura que las imágenes se alineen horizontalmente sin que se apilen */
-            animation: scroll 20s linear infinite;
-        }
-
-        .marquee img {
-            width: 250px;
-            height: 250px;
-            margin-right: 10px; /* Espacio entre las imágenes */
-        }
-
-        @keyframes scroll {
-            from {
-                transform: translateX(0);
-            }
-            to {
-                transform: translateX(-100%);
-            }
-        }
-
         /* Estilo para el botón de ir arriba */
         #scrollToTopBtn {
             position: fixed;
@@ -111,75 +195,12 @@
             border: none;
             cursor: pointer;
         }
-
     </style>
 </head>
 
 <body>
 
     <!-- Contenido de tu página -->
-    <div class="hero">
-        <div class="container text-center">
-            <h1>Bienvenidos a nuestra escuela</h1>
-        </div>
-    </div>
-
-    <section class="carousel">
-        <div class="marquee-container">
-            <div class="marquee-content">
-                <div class="marquee">
-                    <img src="IMG/bienvenida/21septiembre.jpg" alt="21 de Septiembre">
-                    <img src="IMG/bienvenida/albañileria.jpg" alt="Albañilería">
-                    <img src="IMG/bienvenida/carpinteria.png" alt="Carpintería">
-                    <img src="IMG/bienvenida/informatica.jpg" alt="Informática">
-                    <img src="IMG/bienvenida/peluquieria.jpg" alt="Peluquería">
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <br><br>
-    <h2 class="diario-title">Noticias de la Semana</h2>
-    <br>
-
-    <section class="noticias-containersecundario">
-        <article>
-            <img src="/img2.0/construccion.jpg" alt="Proceso de construcción">
-            <h3>Proceso de Construcción</h3>
-            <p>Avances en la construcción nueva aula CFPN2, en articulación con la Municipalidad de Santa María, continuamos hacia delante para concretar la meta de tener un nuevo espacio para la enseñanza. #fpcatamarca #JuntosPodemos</p>
-            <a href="https://www.facebook.com/share/p/1M8sXMLUnH/" target="_blank">Leer más</a>
-        </article>
-        <article>
-            <img src="/img2.0/halloween.jpg" alt="Halloween">
-            <h3>Halloween</h3>
-            <p>Propuesta para niños Taller de Halloween, (hasta 10 años), aprenderán hacer (cupcakes y galletas), días Jueves primer grupo y viernes segundo grupo, desde las 15:00:h. Valor $2.500 incluye insumos. te esperamos Vení disfrazado.!! p/d inscripciones a los números que figuran el el flyer.</p>
-            <a href="https://www.facebook.com/share/p/15MnXWt92y/" target="_blank">Leer más</a>
-        </article>
-    </section>
-
-    <section class="noticias-containersecundario">
-        <article>
-            <img src="/img2.0/silicon.jpg" alt="Silicon Valley">
-            <h3>Experiencia Silicon Valley</h3>
-            <p>Participantes de la Experiencia Silicon Valley 2024 propuestos por la Fundación del Tucumán, con integrantes de Paraguay, Bolivia y Argentina (Salta, Jujuy, Tucumán y Catamarca), Visitas a Google, HP, Plug and Play, Tesla, Stanford University, Amazon, Draper University, Linckein, SAP, Autodesk, Indie Bio, más otras aceleradoras de Startups, tecnológicas y biotecnológicas. Nodo Tecnológico - Catamarca Capital Municipalidad de Santa María #FPCatamarca #CFPN2</p>
-            <a href="https://www.facebook.com/share/p/1EiPfLNCHs/" target="_blank">Leer más</a>
-        </article>
-
-        <article>
-            <img src="/img2.0/diaEstu2.jpg" alt="Jornada día del estudiante">
-            <h3>Jornada Día del Estudiante</h3>
-            <p>Jornada por el Día del estudiante y elección de la Reina y princesas. Agradecemos al complejo policial por brindarnos sus instalaciones.</p>
-            <a href="https://www.facebook.com/share/p/19ChxLjDsX/" target="_blank">Leer más</a>
-        </article>
-
-        <article>
-            <img src="/img2.0/energiaReno.jpg" alt="Energía Renovable">
-            <h3>Energía Renovable</h3>
-            <p>Jornada de capacitación para alumnos IES de la Tecnicatura Superior en Gestión de Energías Renovables, dictado por el Prof. Fernando Paz y alumnos de Electricidad Domiciliaria. Articulación IES/CFPN2.</p>
-            <a href="https://www.facebook.com/share/p/14k2aGB6bG/" target="_blank">Leer más</a>
-        </article>
-    </section>
-
     <div class="moving-text neon-text" id="movingText">
         <p><a href="inscripcion.html" target="_blank">Inscripciones 2025</a></p>
         <button id="closeButton" onclick="closeMovingText()">Cerrar</button>
@@ -192,6 +213,7 @@
     </button>
 
     <script>
+        // Mostrar/ocultar el botón al desplazarse
         window.onscroll = function () {
             scrollFunction();
         };
@@ -199,27 +221,30 @@
         function scrollFunction() {
             var scrollToTopBtn = document.getElementById("scrollToTopBtn");
             if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                scrollToTopBtn.style.display = "flex";
+                scrollToTopBtn.style.display = "flex"; // Mostrar botón
             } else {
-                scrollToTopBtn.style.display = "none";
+                scrollToTopBtn.style.display = "none"; // Ocultar botón
             }
         }
 
+        // Función para volver arriba
         function scrollToTop() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
+            document.body.scrollTop = 0; // Safari
+            document.documentElement.scrollTop = 0; // Chrome, Firefox, IE y Opera
         }
 
+        // Cerrar el texto en movimiento
         function closeMovingText() {
-            document.getElementById('movingText').style.display = 'none';
+            document.getElementById("movingText").style.display = "none";
         }
     </script>
+
+
+    <?php include 'layaout/footer.php';?>
+
+
+    
 
 </body>
 
 </html>
-
-<?php
-    // Incluir pie de página si lo deseas modular
-    include 'layaout/footer.php';
-?>
